d92f0b307d6452f2641b0c2a7a16879b2780e562
SMARTOSC\hongdnn <hongdnn@smartosc.com>
2022-10-25 16:26:10 +0700

    course detail model layout

--- app/src/main/java/com/example/myhome/androidui/online_course_detail/controller/model/OnlineCourseDetailModel.kt
+++ app/src/main/java/com/example/myhome/androidui/online_course_detail/controller/model/OnlineCourseDetailModel.kt
@@ -1,12 +1,19 @@
 package com.example.myhome.androidui.online_course_detail.controller.model
 
+import android.content.Context
 import com.airbnb.epoxy.EpoxyModelClass
-import androidx.viewbinding.ViewBinding
-import com.airbnb.epoxy.EpoxyHolder
-import com.airbnb.epoxy.EpoxyModelWithHolder
 import com.example.myhome.androidui.R
+import com.example.myhome.androidui.base.epoxy.EpoxyViewBindingModelWithHolder
 import com.example.myhome.androidui.databinding.ListItemOnlineCourseDetailBinding
 
 @EpoxyModelClass(layout = R.layout.list_item_online_course_detail)
-class OnlineCourseDetailModel : EpoxyViewB<ListItemOnlineCourseDetailBinding> {
+class OnlineCourseDetailModel :
+    EpoxyViewBindingModelWithHolder<ListItemOnlineCourseDetailBinding>() {
+    override fun ListItemOnlineCourseDetailBinding.bind(context: Context) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDefaultLayout(): Int {
+        TODO("Not yet implemented")
+    }
 }
